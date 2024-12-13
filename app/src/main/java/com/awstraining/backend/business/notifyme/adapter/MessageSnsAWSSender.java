4daf69fafa74fdd25a1d5a2fe2abdf47141f1ecb
Tymoteusz Nowicki <tymoteusz.nowicki@capgemini.com>
2024-12-13 13:43:16 +0100

    sns

--- app/src/main/java/com/awstraining/backend/business/notifyme/adapter/MessageSnsAWSSender.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/adapter/MessageSnsAWSSender.java
@@ -1,6 +1,10 @@
 package com.awstraining.backend.business.notifyme.adapter;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
 import com.awstraining.backend.business.notifyme.MessageSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -10,19 +14,30 @@ public class MessageSnsAWSSender implements MessageSender {
 
     private static final Logger LOGGER = LogManager.getLogger(MessageSnsAWSSender.class);
 
-    // TODO: lab1
-    //  1. Inject AWS AmazonsSNS from configuration SNSConfig.
-    //  2. Make sure that you created new value in parameter store with arn of sns topic.
-    //  3. Inject parameter with @Value annotation through constructor.
-//    @Autowired
-    public MessageSnsAWSSender() {
+    private final AmazonSNS amazonSNS;
+    private final String snsTopicArn;
+
+    // Constructor injection for AmazonSNS and SNS Topic ARN
+    public MessageSnsAWSSender(AmazonSNS amazonSNS, @Value("${notification.topicrn}") String snsTopicArn) {
+        this.amazonSNS = amazonSNS;
+        this.snsTopicArn = snsTopicArn;
     }
 
     @Override
     public void send(String text) {
-        // TODO: lab1
-        //  1. Create publish request.
-        //  2. Publish request with sns.
-        //  3. Log information about successful sent message to topic with topic arn and message id.
+        try {
+            // Create a PublishRequest
+            PublishRequest publishRequest = new PublishRequest()
+                    .withTopicArn(snsTopicArn)
+                    .withMessage(text);
+
+            // Publish the message
+            PublishResult publishResult = amazonSNS.publish(publishRequest);
+
+            // Log successful message publishing
+            LOGGER.info("Message sent to topic ARN: {}. Message ID: {}", snsTopicArn, publishResult.getMessageId());
+        } catch (Exception e) {
+            LOGGER.error("Failed to send message to SNS topic: {}", snsTopicArn, e);
+        }
     }
 }
--- app/src/main/java/com/awstraining/backend/config/SNSConfig.java
+++ app/src/main/java/com/awstraining/backend/config/SNSConfig.java
@@ -1,6 +1,12 @@
 package com.awstraining.backend.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
@@ -22,8 +28,20 @@ public class SNSConfig {
     //  1. Configure AmazonSNS which will be used by fargate within AWS.
     //  2. Make sure that your task role has access to publish action (ecs-task-role-policy).
     //  3. Think how to connect with AWS Service from your local pc. 
-//    @Bean
-//    AmazonSNS configureSNSClient() {
-//      
-//    }
+    @Bean
+    public AmazonSNS configureSNSClient() {
+        AmazonSNSClientBuilder builder = AmazonSNSClientBuilder.standard()
+                .withRegion(awsRegion);
+
+        if (snsAccessKey != null && snsSecretKey != null) {
+            // Use explicit credentials if provided
+            builder.withCredentials(new AWSStaticCredentialsProvider(
+                    new BasicAWSCredentials(snsAccessKey, snsSecretKey)));
+        } else {
+            // Use default credentials provider chain
+            builder.withCredentials(DefaultAWSCredentialsProviderChain.getInstance());
+        }
+
+        return builder.build();
+    }
 }
